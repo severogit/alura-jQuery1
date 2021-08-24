b08359bdd1f240ea68c972e60687a209368d20db
Anderson Severo <severowz@gmail.com>
2021-08-24 16:56:47 -0300

    Modificando a função novaLinha para ser possível remover Placares adicionados

--- js/main.js
+++ js/main.js
@@ -75,10 +75,28 @@ function inserePlacar(){
     var usuario = "svr";
     var numPalavras = $("#contador-palavras").text();
 
-    var linha = "<tr>"+
-                    "<td>"+ usuario +"</td>"+
-                    "<td>"+ numPalavras +"</td>"+
-                "</tr>";
+    var linha = novaLinha(usuario, numPalavras);
+    linha.find(".botao-remover").click(removeLinha)
 
     corpoTabela.prepend(linha);
 };
+
+function novaLinha(usuario, numPalavras){
+    var linha = $("<tr>");
+    var colunaUsuario = $("<td>").text(usuario);
+    var colunaPalavras = $("<td>").text(numPalavras);
+    var colunaRemover = $("<td>");
+
+    var link = $("<a>").addClass("botao-remover").attr("href","#");
+    var icone = $("<i>").addClass("small").addClass("material-icons").text("delete");
+
+    link.append(icone);
+
+    colunaRemover.append(link);
+
+    linha.append(colunaUsuario);
+    linha.append(colunaPalavras);
+    linha.append(colunaRemover);
+    
+    return linha;
+}
