5de5a5022cd01a574d64af2010095f9d2f14777d
Anderson Severo <severowz@gmail.com>
2021-08-24 15:41:14 -0300

    Corrigindo um bug que quando reinicializava o jogo, a borda não correspondia ao status

--- js/reiniciar.js
+++ js/reiniciar.js
@@ -2,10 +2,13 @@ function reiniciaJogo(){
 
     console.log("clik");
     campo.attr("disabled", false);
-    campo.val(" ");
+    campo.val("");
     $("#contador-palavras").text("0");
     $("#contador-caracteres").text("0");
     $("#tempo-digitacao").text(tempoInicial);
     inicializaCronometro();
     campo.toggleClass("campo-desativado");
+    campo.removeClass("borda-vermelha");
+    campo.removeClass("borda-azul");
+    campo.removeClass("borda-invisivel");
     };
