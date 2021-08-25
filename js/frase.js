42cb94bd5fcab608df7c5bc804b320961aa7ecd9
Anderson Severo <severowz@gmail.com>
2021-08-25 13:53:00 -0300

    Configurando botão de requisição de frases

--- js/frase.js
+++ js/frase.js
@@ -1,8 +1,8 @@
 $("#botao-frase").click(fraseAleatoria);
+$("#botao-frase-id").click(buscaFrase);
 
 function fraseAleatoria(){
-    $("#spinner").show();
-
+    $("#spinner").toggle();
     $.get("http://localhost:3000/frases", trocaFraseAleatoria)
     .fail(function(){
         $("#erro").toggle();
@@ -23,4 +23,28 @@ function trocaFraseAleatoria(data){
 
     atualizaTamanhoFrase();
     atualizaTempoInicial(data[numeroAleatorio].tempo);
+}
+
+function buscaFrase(){
+    $("#spinner").toggle();
+    var fraseId = $("#frase-id").val();
+    console.log("id da minah frase:" + fraseId);
+    var dados = {id: fraseId};
+    $.get("http://localhost:3000/frases",dados,trocaFrase)
+    .fail(function(){
+        $("#erro").toggle()
+        setTimeout(function(){
+            $("#erro").toggle();
+            }, 1500);
+    })
+    .always(function(){
+        $("#spinner").toggle();
+    });
+}
+
+function trocaFrase(data){
+    var frase = $(".frase");
+    frase.text(data.texto);
+    atualizaTamanhoFrase();
+    atualizaTempoInicial(data.tempo);
 }
