7bda2a355a23bfd5a1f7568eb71d5622bb64b9dc
Anderson Severo <severowz@gmail.com>
2021-08-25 12:26:51 -0300

    Adicionando funções no botão de mudar frase

---
+++ js/frase.js
@@ -0,0 +1,12 @@
+$("#botao-frase").click(fraseAleatoria);
+
+function fraseAleatoria(){
+    $.get("http://localhost:3000/frases", trocaFraseAleatoria);
+}
+
+function trocaFraseAleatoria(data){
+    var frase = $(".frase");
+    var numeroAleatorio = Math.ceil(Math.random() * data.length);
+
+    frase.text(data[numeroAleatorio].texto);
+}
