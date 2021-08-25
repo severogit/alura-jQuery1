1d566b75082d45153b92dcb92248d5388f45bcf8
Anderson Severo <severowz@gmail.com>
2021-08-25 13:10:32 -0300

    Mostrando o erro  caso requisição ajax falhe

--- js/frase.js
+++ js/frase.js
@@ -1,7 +1,13 @@
 $("#botao-frase").click(fraseAleatoria);
 
 function fraseAleatoria(){
-    $.get("http://localhost:3000/frases", trocaFraseAleatoria);
+    $.get("http://localhost:3000/frases", trocaFraseAleatoria)
+    .fail(function(){
+        $("#erro").toggle();
+        setTimeout(function(){
+            $("#erro").toggle();
+            }, 1500);
+    });
 }
 
 function trocaFraseAleatoria(data){
@@ -9,6 +15,7 @@ function trocaFraseAleatoria(data){
     var numeroAleatorio = Math.ceil(Math.random() * data.length);
 
     frase.text(data[numeroAleatorio].texto);
+
     atualizaTamanhoFrase();
     atualizaTempoInicial(data[numeroAleatorio].tempo);
 }
